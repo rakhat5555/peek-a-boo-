d64e9ba01d81c60e493f5d3047c0ab43b20dd11e
rakhat tutueva and Slater Smith <slater@>
2019-01-15 11:06:39 -0800

    add buttons with distinct themes using javascript

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,39 @@ $(function() {
    $("#initial-show").toggle();
  });
 });
+
+$(function() {
+  $("p2").click(function() {
+   $("#initial-hide-two").toggle();
+   $("#initial-show-two").toggle();
+ });
+});
+
+$(document).ready(function() {
+  $("button#grey").click(function() {
+    $("body").removeClass();
+    $("body").addClass("grey-background");
+    $("span").removeClass("clickable");
+    $("span").removeClass("clickable-yellow");
+    $("span").removeClass("clickable-blue");
+    $("span").addClass("clickable-grey");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+    $("span").removeClass("clickable");
+    $("span").removeClass("clickable-grey");
+    $("span").removeClass("clickable-blue");
+    $("span").addClass("clickable-yellow");
+  });
+
+  $("button#blue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("blue-background");
+    $("span").removeClass("clickable");
+    $("span").removeClass("clickable-grey");
+    $("span").removeClass("clickable-yellow");
+    $("span").addClass("clickable-blue");
+  });
+});
